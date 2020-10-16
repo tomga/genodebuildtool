e6c74f2de70c582dea5558bc735a3a98c23a267a
Tomasz Gajewski <tomga@wp.pl>
2020-10-17 00:16:05 +0200

    mklogparser: fixed parsing of empty library rebuilds

--- mklogparser.py
+++ mklogparser.py
@@ -8,8 +8,8 @@ def initialize():
     grammar = parglare.Grammar.from_file(file_name)
 
     actions = {
-        "CmdList": [lambda _, nodes: [ nodes[0] ],
-                    lambda _, nodes: nodes[0] + [ nodes[1] ],
+        "CmdList": [lambda _, nodes: [ nodes[0] ]  if nodes[0] is not None else [],
+                    lambda _, nodes: nodes[0] + [ nodes[1] ] if nodes[1] is not None else nodes[0],
                     ],
         "Command": [lambda _, nodes: [ [ "Library", nodes[1], "in", nodes[0] ], nodes[2] ],
                     lambda _, nodes: [ [ "Program", nodes[1], "in", nodes[0] ], nodes[2] ],
@@ -17,6 +17,8 @@ def initialize():
                     lambda _, nodes: [ "Nothing in", nodes[0] ],
                     lambda _, nodes: [ nodes[0], nodes[1] ],
                     lambda _, nodes: nodes[0],
+                    lambda _, nodes: None,
+                    lambda _, nodes: None,
                     ],
         "EnteringDir": [lambda _, nodes: nodes[1] ],
         "LeavingDir": [lambda _, nodes: nodes[1] ],
