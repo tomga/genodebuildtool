7d1f4fa7f3a9bb4817f7e574ce8baec2d7784f39
Tomasz Gajewski <tomga@wp.pl>
2020-12-13 14:46:14 +0100

    abi_so: support same set of ld_opts as lib_so
    All required for ld-linux work as expected.

--- tools/genode_abi_so.py
+++ tools/genode_abi_so.py
@@ -24,46 +24,71 @@ def generate(env):
 def exists(env):
     return True
 
+
 def process_T_options(ld_opts, env):
+    return process_file_options(ld_opts, env, '-T', ' ')
 
+def process_version_script_options(ld_opts, env):
+    return process_file_options(ld_opts, env, '--version-script=', '')
+
+def process_file_options(ld_opts, env, option, optsep):
     scripts = []
-
     def scripts_normalize(match):
         match = match.group()
-        script = match[2:].strip()
-        script = env['fn_localize_path'](script)
+        script = match[len(option):].strip()
+        if script.startswith('/'):
+            script = env['fn_localize_path'](script)
+        else:
+            script = env['fn_unsconsify_path'](env['fn_target_path'](script))
         scripts.append(script)
-        return '-T ' + script
+        return option + optsep + script
 
     ld_opts = ' '.join(ld_opts.split())
-    ld_opts = re.sub(r'-T *[^ ]+', scripts_normalize, ld_opts)
+    ld_opts = re.sub(option + r' *[^ ]+', scripts_normalize, ld_opts)
 
     return ld_opts, scripts
 
 
 def abi_so_emitter(target, source, env):
 
-    main_script = env['fn_localize_path'](env['LD_SCRIPT_SO'])
-    ld_opts, scripts = process_T_options(env['LD_OPT'], env)
-    env['fn_debug']('abi_so_emitter: %s %s' % (str(target), str(source + [main_script] + scripts)))
-    return (target, source + [main_script] + scripts)
+    ld_script_so = env['fn_localize_path'](env['LD_SCRIPT_SO']).split()
+    main_script = ld_script_so[0]
+    hidden_opts, hidden_scripts = process_T_options(' '.join(ld_script_so[1:]), env)
+
+    ld_opts = env['LD_OPT']
+    ld_opts, scripts = process_T_options(ld_opts, env)
+    ld_opts, vscripts = process_version_script_options(ld_opts, env)
+
+    env['fn_debug']('abi_so_emitter: %s %s' %
+                    (str(list(map(str, target))),
+                     str(list(map(str, source + [main_script] + hidden_scripts + scripts + vscripts)))))
+    return (target, source + [main_script] + hidden_scripts + scripts + vscripts)
 
 
 def abi_so_generator(target, source, env, for_signature):
 
-    ld_opts, scripts = process_T_options(env['LD_OPT'], env)
+    ld_script_so = env['fn_localize_path'](env['LD_SCRIPT_SO']).split()
+    main_script = ld_script_so[0]
+    hidden_opts, hidden_scripts = process_T_options(' '.join(ld_script_so[1:]), env)
+
+    ld_opts = env['LD_OPT']
+    ld_opts, scripts = process_T_options(ld_opts, env)
+    ld_opts, vscripts = process_version_script_options(ld_opts, env)
+
+    emitter_added_src_count = len(hidden_scripts) + len(scripts) + len(vscripts) + 1
 
     d = { 'ld': env['LD'],
           'abi_so': target[0],
           'abi_soname': os.path.basename(str(target[0]))[0:-len('.abi.so')] + '.lib.so',
           'ld_opts': ld_opts,
-          'ld_script_so': source[1],
+          'ld_script_so': main_script,
+          'ld_script_rest': hidden_opts,
           'lib_so_deps': env['LIB_SO_DEPS'],
           'symbols_obj': source[0],
           }
 
     cmd = r"""{ld} -o {abi_so} -soname={abi_soname} -shared --eh-frame-hdr {ld_opts}
-                   -T {ld_script_so}
+                   -T {ld_script_so} {ld_script_rest}
                    --whole-archive --start-group
                    {lib_so_deps} {symbols_obj}
                    --end-group --no-whole-archive
