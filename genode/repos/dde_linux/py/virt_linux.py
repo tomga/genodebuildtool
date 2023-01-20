ee859a36b75b49d54b15adbd31bbceaef2101f70
Tomasz Gajewski <tomga@wp.pl>
2023-01-20 22:23:28 +0100

    overlays virt_linux: ccache support

--- genode/repos/dde_linux/py/virt_linux.py
+++ genode/repos/dde_linux/py/virt_linux.py
@@ -27,9 +27,21 @@ class GenodeDdeLinuxVirtMkProg(genode_prog.GenodeMkProg):
         sc_tgt_file = env['fn_norm_tgt_path']('kernel_config.tag')
         lx_dir = env['fn_localize_path'](build_env.var_value('LX_DIR'))
 
+        def update_lx_mk_args(lx_mk_args):
+            """If CC is set in args and relative path points to build directory
+               then it has to be replaced to absolute for linux build"""
+            build_dir = env['BUILD']
+            abs_build_dir = os.path.abspath(build_dir)
+            rel_cc_prefix = 'CC=%s' % build_dir
+            abs_cc_prefix = 'CC=%s' % abs_build_dir
+            return lx_mk_args.replace(rel_cc_prefix, abs_cc_prefix)
+
+        lx_mk_args = build_env.var_value('LX_MK_ARGS')
+        lx_mk_args = update_lx_mk_args(lx_mk_args)
+
         conf_tgt = env.LinuxKTag(target = sc_tgt_file,
                                  lx_dir = lx_dir,
-                                 lx_mk_args = build_env.var_value('LX_MK_ARGS'),
+                                 lx_mk_args = lx_mk_args,
                                  lx_enable = build_env.var_values('LX_ENABLE'),
                                  lx_disable = build_env.var_values('LX_DISABLE'),
                                  without_prepare = self.lx_ktag_without_prepare,
@@ -37,7 +49,7 @@ class GenodeDdeLinuxVirtMkProg(genode_prog.GenodeMkProg):
 
         bzimage_tgt = env.LinuxBuild(source = conf_tgt,
                                      lx_dir = lx_dir,
-                                     lx_mk_args = build_env.var_value('LX_MK_ARGS'),
+                                     lx_mk_args = lx_mk_args,
                                      lx_target = self.lx_target,
                                      )
 
